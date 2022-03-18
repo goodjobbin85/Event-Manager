34fba45828095df9eff7f9c49476dfa23afc2624
Thomas Dubiel <td1729@hotmail.com>
2022-03-18 09:51:45 -0500

    cleanup and parse zip_code

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,8 +2,23 @@ require 'csv'
 
 puts "Welcome to Event Manager!" 
 
-content = CSV.open("attendees.csv", headers: true)
+content = CSV.open(
+    "attendees.csv",
+     headers: true, 
+     header_converters: :symbol 
+) 
+
 content.each do |line| 
-    name = line[2]
-    p name
+    name = line[:first_name] 
+    zip_code = line[:zipcode] 
+
+    if zip_code.nil? 
+        zip_code = '00000' 
+    elsif zip_code.length < 5 
+        zipcode = zip_code.rjust(5, '0') 
+    elsif zip_code.length > 5 
+        zip_code = zip_code[0..4] 
+    end 
+
+    puts "#{name}: #{zip_code}"
 end 
